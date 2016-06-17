62a8c9c6df0fb60bca27b25cb7bc613f3201650c
ammarm08 <ammar.mian08@gmail.com>
2016-06-16 20:17:44 -0400

    rejigger amazon scraper to only do one query at a time

--- helpers/amazon_fetcher.js
+++ helpers/amazon_fetcher.js
@@ -10,10 +10,10 @@ let fs = require('fs');
 function getResponse(msg, cb) {
   Google(msg + 'site:' + SITE, function (err, res) {
     if (err) {
-      return cb(err);
+      return cb(err, null);
     } else {
       let results = res.links[0].href;
-      return cb(results);
+      return cb(null, results);
     }
   })
 }
@@ -23,11 +23,40 @@ fs.readFile('./data/remaining.tsv', 'utf8', function(err, data) {
     console.error(err);
   } else {
     var lines = data.split('\r\n');
-    lines.forEach(function(line) {
-      var msg = line.replace('\t', ' ');
-      getResponse(msg, function(l) {
-        console.log(msg + '\t' + l);
-      })
+    var current = lines.shift().replace('\t', ' ');
+    getResponse(current, function(err, link) {
+      if (err) {
+        return console.error(err);
+      } else {
+        fs.appendFileSync('./data/done.tsv', current + '\t' + link + '\n');
+        fs.writeFileSync('./data/remaining.tsv', lines.join('\r\n'));
+        console.log(current + '\t' + link);
+      }
     })
   }
 })
+
+function fetch_stagger (list) {
+  var copy = list.slice();
+  
+  function step () {
+    var current = copy.shift();
+    if (current) {
+      (function (k) {
+        setTimeout(function() {
+          var msg = k.replace('\t', ' ');
+          getResponse(msg, function(e,l) {
+            if (e) {
+              console.error(e);
+            } else {
+              console.log(msg + '\t' + l);
+            }
+            step();
+          })
+        }, 5000)
+      })(current)
+    }
+  }
+  
+  step();
+}
