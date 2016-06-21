8681a89947d75704fab3acb0565392f1c417eaf2
ammarm08 <ammar.mian08@gmail.com>
2016-06-21 09:25:50 -0400

    update modal button text and tweak scoring formula

--- node/helpers/list_processing.js
+++ node/helpers/list_processing.js
@@ -88,20 +88,8 @@ exports.process_books = function () {
     let link = BOOKS[book].amazon_link;
     let thumbnails = recommenders.map(function(person) { return THUMBNAILS[person]; });
 
-    // adjust for the Sam Harris Effect
-    let frequency = recommenders.reduce(function(total, rec) {
-      let current = RECS[rec].recommended_books.length;
-      if (current > 10) {
-        total += 1/current;
-      } else {
-        total += 1;
-      }
-
-      return total;
-    }, 0)
-
     // calculate score
-    let score = (frequency * 10) + (AUTHORS[author].recommenders.length * 2) + (1000/year) + (100/length);
+    let score = (recommenders.length * 5) + (AUTHORS[author].recommenders.length * 0.5) + (100/year) + (10/length);
 
     var book_obj = {
       title: title,
