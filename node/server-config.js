824d9bc8ac6c9438931d199eb1b2a06d3fba8446
ammarm08 <ammar.mian08@gmail.com>
2016-06-28 17:19:17 -0400

    ssl encrypted nodejs

---
+++ node/server-config.js
@@ -0,0 +1,63 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+
+/* This ensures that we are only working on a secure connection */
+function ensureSecure(req, res, next){
+  if (req.secure) {
+    return next();
+  };
+  res.redirect('https://' + req.hostname + ':' + 8443 + req.url);
+};
+
+/* JSON files */
+let alpha_author = require('./data/alphabetized_author.json');
+let alpha_title = require('./data/alphabetized_title.json');
+let freq_author = require('./data/frequencies_author.json');
+let freq_title = require('./data/frequencies_title.json');
+let recommenders_list = require('./data/recommenders_list.json');
+
+
+/* CONFIGURATION */
+app.all('*', ensureSecure);
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+/* ROUTES */
+app.get('/', function (req, res) {
+  res.render('index', {page_name: 'books', data: freq_title.slice(0, 15)});
+})
+
+app.get('/books', function (req, res) {
+  res.render('index', {page_name: 'books', data: freq_title.slice(0, 15)});
+})
+
+app.get('/thinkers', function (req, res) {
+  res.render('thinkers', {page_name: 'thinkers'});
+})
+
+app.get('/api/books', function (req, res) {
+  var query = req.query.sort;
+
+  if (query === 'frequencies_author') {
+    res.json(freq_author);
+  } else if (query === 'alphabetized_title') {
+    res.json(alpha_title);
+  } else if (query === 'alphabetized_author') {
+    res.json(alpha_author);
+  } else {
+    res.json(freq_title);
+  }
+})
+
+app.get('/api/recommenders', function (req, res) {
+  res.json(recommenders_list);
+})
+
+app.get('/*', function (req, res) {
+  res.redirect('/');
+})
+
+module.exports = app;
--- node/server.js
+++ node/server.js
@@ -1,53 +1,28 @@
 'use strict';
 
-let express = require('express');
-let app = express();
-const PORT = 8080;
+let debug = require('debug')('https-server');
+let app = require('./server-config.js');
+let https = require('https');
+let http = require('http');
+let fs = require('fs');
 
-let alpha_author = require('./data/alphabetized_author.json');
-let alpha_title = require('./data/alphabetized_title.json');
-let freq_author = require('./data/frequencies_author.json');
-let freq_title = require('./data/frequencies_title.json');
-let recommenders_list = require('./data/recommenders_list.json');
+let config = {
+    key: fs.readFileSync('../nginx/ssl/private/bookswell.key'),
+    cert: fs.readFileSync('../nginx/ssl/certs/www_bookswell_io.certchain.crt')
+};
 
-app.set('views', __dirname + '/public/views');
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+let httpPort = 8080;
+let httpsPort = 8443;
 
-app.get('/', function (req, res) {
-  res.render('index', {page_name: 'books', data: freq_title.slice(0, 15)});
-})
+/* Two server instances. HTTP server redirects to HTTPS server */
+let sslEncryptedServer = function() {
+  http.createServer(app).listen(httpPort, function() {
+    console.log("HTTP listening on", httpPort);
+  });
 
-app.get('/books', function (req, res) {
-  res.render('index', {page_name: 'books', data: freq_title.slice(0, 15)});
-})
+  https.createServer(config, app).listen(httpsPort, function() {
+    console.log("HTTPS securely listening on", httpsPort);
+  });
+};
 
-app.get('/thinkers', function (req, res) {
-  res.render('thinkers', {page_name: 'thinkers'});
-})
-
-app.get('/api/books', function (req, res) {
-  var query = req.query.sort;
-
-  if (query === 'frequencies_author') {
-    res.json(freq_author);
-  } else if (query === 'alphabetized_title') {
-    res.json(alpha_title);
-  } else if (query === 'alphabetized_author') {
-    res.json(alpha_author);
-  } else {
-    res.json(freq_title);
-  }
-})
-
-app.get('/api/recommenders', function (req, res) {
-  res.json(recommenders_list);
-})
-
-app.get('/*', function (req, res) {
-  res.redirect('/');
-})
-
-app.listen(PORT, function () {
-  console.log('Listening on port', PORT);
-})
+sslEncryptedServer();
