140009f3159c6f232a882df87f2ad3298b343010
ammarm08 <ammar.mian08@gmail.com>
2016-07-07 12:22:38 -0400

    run background fetch to populate modals in thinkers page

--- node/src/scripts/thinkers.js
+++ node/src/scripts/thinkers.js
@@ -3,49 +3,51 @@ window.initThinkersPage = function() {
     // GLOBALS
     var all_data = [];
     var $recs = $('.recs');
+    
+    $('.thumbnail-container').on('click', updateRecModal);
+    background_fetch();
 
-    // BUILD INDIVIDUAL BOOK ITEM & APPEND IT TO LIST
-    function append_one_rec_to_table(rec) {
-      var $grid = $('<div class="recommender-collection" style="display: none"></div>');
-      var $recommender = $('<div class="recommender" data-toggle="modal" data-target="#recModal"></div>');
-      $recommender.text(rec.name);
-
-      var $thumbContainer = $('<div class="thumbnail-container" data-toggle="modal" data-target="#recModal"></div>');
-      var $thumb = $('<img class="img-circle"/>');
-      $thumb.attr('src', rec.thumbnail);
-
-      $thumbContainer.append($thumb);
-      $grid.append($thumbContainer).append($recommender);
-
-      $grid.on('click', function(e) {
-        e.preventDefault();
-
-        $('.modal-title').text(rec.name + "'s Picks");
-        $('.modal-about').text(rec.name + ' ' + rec.bio);
-
-        $('.recommendations-container').empty();
-        rec.recommended_books.forEach(function(b) {
-          var $rec_group = $('<div></div>');
-          var $recommendation = $('<a class="list-group-item thumbnail recommendation"></a>');
-          var $rec_src = $('<a class="glyphicon glyphicon-question-sign"></a>');
-
-          $recommendation.text(b[0]);
-          $recommendation.attr('href', b[1]);
-          $recommendation.attr('target', '_blank');
-
-          $rec_src.attr('title', 'Source');
-          $rec_src.attr('href', b[2]);
-          $rec_src.attr('target', '_blank');
-          $rec_src.data('toggle', 'tooltip');
-          $rec_src.tooltip();
-
-          $rec_group.append($recommendation).append($rec_src);
-          $('.recommendations-container').append($rec_group);
-        })
-      });
-
-      $books.append($grid);
-      $grid.fadeIn(1000);
+    function background_fetch () {
+      $.ajax({
+        type: 'GET',
+        url: '/api/recommenders',
+        success: function(data) {
+          all_data = data;
+          setDataAttributesForRecs();
+        }
+      })
     }
-  })
+
+    function setDataAttributesForRecs () {
+      $('.thumbnail-container').each(function(i) {
+        var $self = $(this);
+        $self.data('recommended-books', all_data[i].recommended_books);
+      });
+    }
+
+    function updateRecModal (e) {
+      $('.modal-title').text($(this).data('name') + "'s Picks");
+      $('.modal-about').text($(this).data('name') + ' ' + $(this).data('bio'));
+
+      var books = $(this).data('recommended-books');
+      for (var i = 0; i < books.length; i++) {
+        var $rec_group = $('<div></div>');
+        var $recommendation = $('<a class="list-group-item thumbnail recommendation"></a>');
+        var $rec_src = $('<a class="glyphicon glyphicon-question-sign"></a>');
+
+        $recommendation.text(books[i][0]);
+        $recommendation.attr('href', books[i][1]);
+        $recommendation.attr('target', '_blank');
+
+        $rec_src.attr('title', 'Source');
+        $rec_src.attr('href', books[i][2]);
+        $rec_src.attr('target', '_blank');
+        $rec_src.data('toggle', 'tooltip');
+        $rec_src.tooltip();
+
+        $rec_group.append($recommendation).append($rec_src);
+        $('.recommendations-container').append($rec_group);
+      }
+    }
+  });
 };
