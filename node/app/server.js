d4997b9086aee5393c5c88bff62363db8fb36002
ammarm08 <ammar.mian08@gmail.com>
2016-07-21 15:54:09 -0400

    use cluster module to serve app across several processes

--- node/app/server.js
+++ node/app/server.js
@@ -1,16 +1,61 @@
 'use strict';
 
-let debug = require('debug')('https-server');
 let app = require('./server-config.js');
-let https = require('https');
-let http = require('http');
-let fs = require('fs');
-
+let cluster = require('cluster');
 const PORT = 8080;
 
-app.listen(PORT, function () {
-  console.log('Listening on: ', PORT);
-})
+function restartWorkers () {
+  let workerIds = [];
 
+  for (let wid in cluster.workers) {
+    workerIds.push(wid);
+  }
 
+  workerIds.forEach(function (wid) {
+    cluster.workers[wid].send({
+      text: 'shutdown',
+      from: 'master'
+    });
 
+    setTimeout(function () {
+      if (cluster.workers[wid]) {
+        cluster.workers[wid].kill('SIGKILL');
+      }
+    }, 5000);
+  });
+}
+
+if (cluster.isMaster) {
+  let numWorkers = require('os').cpus().length;
+  let fs = require('fs');
+  let worker;
+
+  console.log('Master cluster setting up ' + numWorkers + 'workers...');
+
+  for (let i = 0; i < numWorkers; i++) {
+    worker = cluster.fork();
+    worker.on('message', function () {
+      console.log('arguments', arguments);
+    });
+  }
+
+  cluster.on('exit', function (_worker, code, signal) {
+    console.log('Worker ' + _worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+    console.log('Starting a new worker');
+
+    worker = cluster.fork();
+    worker.on('message', function() {
+      console.log('arguments', arguments);
+    });
+  });
+} else {
+  process.on('message', function (message) {
+    if (message.type === 'shutdown') {
+      process.exit(0);
+    }
+  });
+
+  app.listen(PORT, function () {
+    console.log('Worker ' + process.pid + ' is alive and listening on ', PORT);
+  });
+}
