0a9772e4055a86f801d609b82293beab3a88d59e
ammarm08 <ammar.mian08@gmail.com>
2016-07-22 13:13:54 -0400

    refactor cluster code to respawn on exit

--- node/app/server.js
+++ node/app/server.js
@@ -4,58 +4,29 @@ let app = require('./server-config.js');
 let cluster = require('cluster');
 const PORT = 8080;
 
-function restartWorkers () {
-  let workerIds = [];
-
-  for (let wid in cluster.workers) {
-    workerIds.push(wid);
+function launchCluster () {
+  if (cluster.isMaster) {
+    let numWorkers = require('os').cpus().length;
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+    createWorkers(numWorkers);
+  } else {
+    app.listen(PORT, function () { console.log('Listening on port', PORT); });
   }
-
-  workerIds.forEach(function (wid) {
-    cluster.workers[wid].send({
-      text: 'shutdown',
-      from: 'master'
-    });
-
-    setTimeout(function () {
-      if (cluster.workers[wid]) {
-        cluster.workers[wid].kill('SIGKILL');
-      }
-    }, 5000);
-  });
 }
 
-if (cluster.isMaster) {
-  let numWorkers = require('os').cpus().length;
-  let fs = require('fs');
-  let worker;
-
-  console.log('Master cluster setting up ' + numWorkers + 'workers...');
-
-  for (let i = 0; i < numWorkers; i++) {
-    worker = cluster.fork();
-    worker.on('message', function () {
-      console.log('arguments', arguments);
-    });
+function createWorkers (n) {
+  for (let i = 0; i < n; i++) {
+    createWorker();
   }
-
-  cluster.on('exit', function (_worker, code, signal) {
-    console.log('Worker ' + _worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-    console.log('Starting a new worker');
-
-    worker = cluster.fork();
-    worker.on('message', function() {
-      console.log('arguments', arguments);
-    });
-  });
-} else {
-  process.on('message', function (message) {
-    if (message.type === 'shutdown') {
-      process.exit(0);
-    }
-  });
-
-  app.listen(PORT, function () {
-    console.log('Worker ' + process.pid + ' is alive and listening on ', PORT);
-  });
 }
+
+function createWorker () {
+  let worker = cluster.fork();
+  worker.on('exit', function (w, code, sig) {
+    console.log('Worker ' + w.process.pid + ' died with code: ' + code + ', and signal: ' + sig);
+    console.log('Spinning up new worker');
+    createWorker(); // respawn on exit
+  })
+}
+
+launchCluster();
