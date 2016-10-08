354f93428296ca76dfc92a8aa6f4fcdb8b6649a8
ammarm08 <ammar.mian08@gmail.com>
2016-10-08 16:07:17 -0700

    comment, es6ify, and clean up variable names

--- node/helpers/amazon_fetcher.js
+++ node/helpers/amazon_fetcher.js
@@ -1,62 +1,70 @@
 'use strict'
 
-const SITE = 'amazon.com';
+const SITE = 'amazon.com'
+const fs = require('fs')
 
-let Google = require('google');
-Google.resultsPerPage = 3;
+let Google = require('google')
+Google.resultsPerPage = 3
 
-let fs = require('fs');
+// search for a book on google and limit results to those from amazon.
+// callback returns the first link found, else an empty array.
+const getBookLink = (book, cb) => {
+  const query = book + 'site:' + SITE
 
-function getResponse(msg, cb) {
-  Google(msg + 'site:' + SITE, function (err, res) {
+  Google(query, (err, res) => {
     if (err) {
-      return cb(err, null);
-    } else {
-      let results = res.links[0] ? res.links[0].href : [];
-      return cb(null, results);
+      return cb(err, null)
     }
+
+    const result = res.links[0] ? res.links[0].href : []
+    return cb(null, result)
   })
 }
 
-fs.readFile('./data/remaining.tsv', 'utf8', function(err, data) {
+// Remaining: list of books left to scrape.
+// Done: list of books already scraped.
+// When a book is successfully scraped, it is removed from remaining and added to done.
+fs.readFile('./data/remaining.tsv', 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-  } else {
-    var lines = data.split('\r\n');
-    var current = lines.shift().replace('\t', ' ');
-    getResponse(current, function(err, link) {
-      if (err) {
-        return console.error(err);
-      } else {
-        fs.appendFileSync('./data/done.tsv', current + '\t' + link + '\n');
-        fs.writeFileSync('./data/remaining.tsv', lines.join('\r\n'));
-        console.log(current + '\t' + link);
-      }
-    })
+    return console.error(err)
   }
+
+  const books = data.split('\r\n')
+  const book = books[0].replace('\t', ' ') // book to scrape.
+  const remaining = books.slice(1)
+
+  getBookLink(book, (err, link) => {
+    if (err) {
+      return console.error(err)
+    }
+
+    fs.appendFileSync('./data/done.tsv', book + '\t' + link + '\n')
+    fs.writeFileSync('./data/remaining.tsv', remaining.join('\r\n'))
+    console.log(book + '\t' + link)
+  })
 })
 
-function fetch_stagger (list) {
-  var copy = list.slice();
-  
-  function step () {
-    var current = copy.shift();
-    if (current) {
-      (function (k) {
-        setTimeout(function() {
-          var msg = k.replace('\t', ' ');
-          getResponse(msg, function(e,l) {
-            if (e) {
-              console.error(e);
-            } else {
-              console.log(msg + '\t' + l);
-            }
-            step();
-          })
-        }, 5000)
-      })(current)
-    }
-  }
-  
-  step();
-}
+/* TODO: async calls for batch scraping */
+// const fetch_stagger = (list) => {
+//   var copy = list.slice();
+
+//   function step () {
+//     var current = copy.shift();
+//     if (current) {
+//       (function (k) {
+//         setTimeout(function() {
+//           var msg = k.replace('\t', ' ');
+//           getResponse(msg, function(e,l) {
+//             if (e) {
+//               console.error(e);
+//             } else {
+//               console.log(msg + '\t' + l);
+//             }
+//             step();
+//           })
+//         }, 5000)
+//       })(current)
+//     }
+//   }
+//   step();
+// }
